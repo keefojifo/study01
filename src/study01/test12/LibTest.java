5b84be9098ab9b72d2e8221f8eb9a3d3542a0cbb
Administrator <Administrator@koitt03a-PC>
2019-10-14 17:51:49 +0900

    2019.10.14

--- src/study01/test12/LibTest.java
+++ src/study01/test12/LibTest.java
@@ -1,29 +1,26 @@
-package study01.test12;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-
-import org.apache.commons.lang3.StringUtils;
-
-public class LibTest {
-
-	public static void main(String[] args) {
-
-		List<String> ages = new ArrayList<String>();
-		Scanner scan = new Scanner(System.in);
-		for (int i = 1; i <= 3; i++) {
-			System.out.println("나이를 입력해주세요 :");
-			String age = scan.nextLine();
-			age = StringUtils.leftPad(age,3,"0"); //StringUtils에 왠만한 스트링 유틸이 있다.
-			ages.add(age);
-		}
-		Collections.sort(ages);
-
-		for (int i = 0; i < ages.size(); i++) {
-			System.out.println(ages.get(i));
-
-		}
-	}
-}
+/*
+ * package study01.test12;
+ * 
+ * import java.util.ArrayList; import java.util.Collections; import
+ * java.util.List; import java.util.Scanner;
+ * 
+ * //import org.apache.commons.lang3.StringUtils;
+ * 
+ * 
+ * 
+ * 
+ * 
+ * public class LibTest {
+ * 
+ * public static void main(String[] args) {
+ * 
+ * List<String> ages = new ArrayList<String>(); Scanner scan = new
+ * Scanner(System.in); for (int i = 1; i <= 3; i++) {
+ * System.out.println("나이를 입력해주세요 :"); String age = scan.nextLine(); age =
+ * //StringUtils.leftPad(age,3,"0"); //StringUtils에 왠만한 스트링 유틸이 있다.
+ * ages.add(age); } Collections.sort(ages);
+ * 
+ * for (int i = 0; i < ages.size(); i++) { System.out.println(ages.get(i));
+ * 
+ * } } }
+ */
